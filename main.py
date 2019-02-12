788163f983f1cc8850b6c0c21ebdd85d0075c521
Don Jayamanne <don.jayamanne@yahoo.com>
2019-02-12 02:36:59 -0800

    Updates

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 
+import somePackage.wow
 
 def do_something():
 	for i in range(2):
 		print(i)
+	somePackage.wow.say_wow()
+	print('bye')
 
 print('Application launched successfully')
 
---
+++ somePackage/wow.py
@@ -0,0 +1,7 @@
+
+
+
+
+
+def say_wow():
+	print('wow')
